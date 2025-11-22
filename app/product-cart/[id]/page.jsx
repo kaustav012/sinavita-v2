77cbdf2db1c2288e78077172afe8e8fef8ccc81b
valktechnologies40 <valktech012@gmail.com>
2025-11-22 11:42:57 +0530

    updated path

--- app/product-cart/[id]/page.jsx
+++ app/product-cart/[id]/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 import { Button } from "../../../components/ui/button";
-import Header from "../../components/header";
+import Header from "../../../components/header";
 import Footer from "../../../components/footer";
+
 import { useState, useEffect } from "react";
 import { RadioGroup, RadioGroupItem } from "../../../components/ui/radio-group";
 import { Label } from "../../../components/ui/label";
